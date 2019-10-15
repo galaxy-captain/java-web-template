d7faa34462d2cddcf1404dc8393c99d45f383513
galaxy-captain <goskkyer@gmail.com>
2019-10-15 21:38:59 +0800

    超级脚手架

---
+++ framework-config/src/main/java/me/galaxy/framework/config/ConfigApplication.java
@@ -0,0 +1,20 @@
+package me.galaxy.framework.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description
+ * @Author duanxiaolei@bytedance.com
+ * @Date 2019-10-15 21:35
+ **/
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
